406fb909ce4f9d59c147d862e8cd8887efd107f0
eddiechapman <eddie.chapman4@gmail.com>
2018-12-01 15:21:04 -0600

    change classes to one "organization" class to accommodate organizations with multiple roles

--- models/organizations.py
+++ models/organizations.py
@@ -3,7 +3,7 @@ from py2neo.ogm import Property, GraphObject, Label, RelatedFrom, RelatedTo
 from models.film import Film
 
 
-class Distributor(GraphObject):
+class Organization(GraphObject):
     """
     Represents a distribution company in the San Francisco Film Locations dataset.
     """
@@ -11,24 +11,23 @@ class Distributor(GraphObject):
 
     name = Property()
     organization = Label()
-    distributor = Label()
 
-    distributed = RelatedTo(Film)
-    distributed_by = RelatedFrom(Film)
-
-
-class Producer(GraphObject):
-    """
-    Represents a production company in the San Francisco Film Locations dataset.
-    """
-    __primarykey__ = 'name'
-
-    name = Property()
-    organization = Label()
-    producer = Label()
-
-    organization = True
-    producer = True
-
-    produced = RelatedTo(Film)
-    produced_by = RelatedFrom(Film)
+#     distributed = RelatedTo(Film)
+#     distributed_by = RelatedFrom(Film)
+#
+#
+# class Producer(GraphObject):
+#     """
+#     Represents a production company in the San Francisco Film Locations dataset.
+#     """
+#     __primarykey__ = 'name'
+#
+#     name = Property()
+#     organization = Label()
+#     producer = Label()
+#
+#     organization = True
+#     producer = True
+#
+#     produced = RelatedTo(Film)
+#     produced_by = RelatedFrom(Film)
