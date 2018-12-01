91471158d86dc0896634c183fe53460ecb7a1f9c
eddiechapman <eddie.chapman4@gmail.com>
2018-12-01 15:09:51 -0600

    create films function

--- import_csv.py
+++ import_csv.py
@@ -2,6 +2,7 @@ from csv import DictReader
 import os
 
 from py2neo import Graph
+from .film import Film
 from .people import Actor, Director, Writer
 from .organizations import Producer, Distributor
 from .locations import Location
@@ -19,16 +20,38 @@ def set_working_directory():
 
 def list_films(reader):
     """
-    Create a list of unique film titles and their release years.
+    Create a dictionary of unique film titles and their release years.
     Avoids cases where a film is associated with multiple release years.
     """
     films = {}
     for row in reader:
         title, release_year = row[:3]
         if title not in films:
-            films[title] = release_year
+            films[title] = {'title': title, 'release_year': release_year}
 
     return films
+
+
+def create_films(films):
+    """
+    Create node objects for the deduplicated films.
+    """
+    film_nodes = []
+
+    for film_info in films.keys:
+        film = Film(film_info)
+        film.title = films['title']
+        film.release_year = int(films['release_year'])
+        film.film = True
+        film_nodes.append(film)
+
+    return film_nodes
+
+
+
+def list_film_location_and_fact(reader):
+    film_location_and_fact = {}
+
 
 
 if __name__ == '__main__':
@@ -40,4 +63,5 @@ if __name__ == '__main__':
 
     with open('Film_Locations_In_San_Francisco.csv') as csv_file:
         reader = DictReader(csv_file)
+        films = list_films(reader)
 
