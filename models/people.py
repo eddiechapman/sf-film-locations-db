3d2b1d94751c1aa8d31738f4914931aa39222dc7
eddiechapman <eddie.chapman4@gmail.com>
2018-12-01 15:20:24 -0600

    change classes to one "people" class to accomidate people with multiple roles

--- models/people.py
+++ models/people.py
@@ -3,7 +3,7 @@ from py2neo.ogm import Property, GraphObject, Label, RelatedFrom, RelatedTo
 from models.film import Film
 
 
-class Actor(GraphObject):
+class Person(GraphObject):
     """
     Represents an actor in the San Francisco Film Locations dataset.
     """
@@ -11,35 +11,35 @@ class Actor(GraphObject):
 
     name = Property()
     person = Label()
-    actor = Label()
-
-    starred_in = RelatedTo(Film)
-    starring = RelatedFrom(Film)
-
-
-class Writer(GraphObject):
-    """
-    Represents a writer in the San Francisco Film Locations dataset.
-    """
-    __primarykey__ = 'name'
-
-    name = Property()
-    person = Label()
-    writer = Label()
-
-    wrote = RelatedTo(Film)
-    written_by = RelatedFrom(Film)
-
-
-class Director(GraphObject):
-    """
-    Represents a director in the San Francisco Film Locations dataset.
-    """
-    __primarykey__ = 'name'
-
-    name = Property()
-    person = Label()
-    director = Label()
-
-    directed = RelatedTo(Film)
-    directed_by = RelatedFrom(Film)
+#     actor = Label()
+#
+#     starred_in = RelatedTo(Film)
+#     starring = RelatedFrom(Film)
+#
+#
+# class Writer(GraphObject):
+#     """
+#     Represents a writer in the San Francisco Film Locations dataset.
+#     """
+#     __primarykey__ = 'name'
+#
+#     name = Property()
+#     person = Label()
+#     writer = Label()
+#
+#     wrote = RelatedTo(Film)
+#     written_by = RelatedFrom(Film)
+#
+#
+# class Director(GraphObject):
+#     """
+#     Represents a director in the San Francisco Film Locations dataset.
+#     """
+#     __primarykey__ = 'name'
+#
+#     name = Property()
+#     person = Label()
+#     director = Label()
+#
+#     directed = RelatedTo(Film)
+#     directed_by = RelatedFrom(Film)
