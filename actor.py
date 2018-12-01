beaab18e0e7bcfe6a3b9c8429b4ce000d895746d
eddiechapman <eddie.chapman4@gmail.com>
2018-12-01 13:13:26 -0600

    actor class

--- actor.py
+++ actor.py
@@ -1,1 +1,18 @@
-from py2neo.ogm import Property, GraphObject, RelatedTo
+from film import Film
+from py2neo.ogm import Property, GraphObject, Label, RelatedTo
+
+
+class Actor(GraphObject):
+    '''
+    Represents a film in the San Francisco Film Locations dataset.
+    '''
+    __primarykey__ = 'name'
+
+    name = Property()
+    person = Label()
+    actor = Label()
+
+    person = True
+    actor = True
+
+    starred_in = RelatedTo(Film)
