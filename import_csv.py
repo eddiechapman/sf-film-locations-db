0e6eb04ff19de88e5cbb33adf68b437bfb318601
eddiechapman <eddie.chapman4@gmail.com>
2018-12-01 18:43:57 -0600

    refactor to avoid using py2neo object model

--- import_csv.py
+++ import_csv.py
@@ -1,12 +1,6 @@
 from csv import DictReader
 import os
-
-from py2neo import Graph
-from .film import Film
-from .people import Person
-from .organizations import Organization
-from .locations import Location
-from .facts import FunFact
+from py2neo import Graph, Node, NodeMatcher, Relationship
 
 
 def set_working_directory():
@@ -18,120 +12,99 @@ def set_working_directory():
     os.chdir(dir_name)
 
 
-def list_films(reader):
+def find_or_create_node(label, key, value):
     """
-    Create a dictionary of unique film titles and their release years.
-    Avoids cases where a film is associated with multiple release years.
+    Return a node by retrieving or creating one using CSV values.
+    Returns none when CSV cell value is blank.
+    New nodes are merged with the database.
     """
-    films = {}
-    for row in reader:
-        title, release_year = row[:3]
-        if title not in films:
-            films[title] = {'title': title, 'release_year': release_year}
-    return films
+    if value is None:
+        return None
+    match = matcher.match(label, key=value)
+    if match.first() is None:
+        node = Node(label, key=value)
+        transaction.merge(node, primary_label=label, primary_key=key)
+    else:
+        node = match.first()
+    return node
 
 
-def list_people(reader):
-    people = set()
-    for row in reader:
-        people.add(row['director'])
-        people.add(row['writer'])
-        people.add(row['actor_1'])
-        people.add(row['actor_2'])
-        people.add(row['actor_3'])
-    return list(people)
-
-
-def list_organizations(reader):
-    organizations = set()
-    for row in reader:
-        organizations.add(row['producer'])
-        organizations.add(row['distributor'])
-    return list(organizations)
-
-
-def list_locations(reader):
-    locations = set(row['locations'] for row in reader)
-    return list(locations)
-
-
-def list_fun_facts(reader):
-    fun_facts = set(row['locations'] for row in reader)
-    return list(fun_facts)
-
-
-def create_films(films):
+def add_label(node, label):
     """
-    Create node objects for the deduplicated films.
+    Add a secondary label to an existing node.
+
+    Check if node is not null from search/creation function and that
+    label is not already applied.
     """
-    for film_info in films.keys:
-        film = Film(film_info)
-        film.title = films['title']
-        film.release_year = int(films['release_year'])
-        film.film = True
-
-
-def create_people(people):
-    for name in people:
-        person = Person()
-        person.name = name
-        person.person = True
-
-
-def create_organizations(organizations):
-    for name in organizations:
-        organization = Organization()
-        organization.name = name
-        organization.organization = True
-
-
-def create_locations(locations):
-    for name in locations:
-        location = Location()
-        location.name = name
-        location.location = True
-
-
-def label_node(graph, name, label):
-    """
-    Label a node if the node exists and does not already have the label.
-    """
-    node = graph.nodes.get(name)
     if node is None or label in node.labels:
         return
     node.labels.add(label)
+    transaction.merge(node, primary_label=label)
 
 
-def assign_labels(reader, graph):
+def create_relationship(node1, relationship_type, node2):
     """
-    Iterate through dataset and assign labels to entities based on column.
+    Add a relationship to two nodes as long as neither are null.
     """
-    for row in reader:
-        label_node(graph, row['producer'], 'producer')
-        label_node(graph, row['distributor'], 'distributor')
-        label_node(graph, row['director'], 'director')
-        label_node(graph, row['writer'], 'writer')
-        label_node(graph, row['actor_1'], 'actor')
-        label_node(graph, row['actor_2'], 'actor')
-        label_node(graph, row['actor_3'], 'actor')
+    if node1 is None or node2 is None:
+        return
+    relationship = Relationship(node1, relationship_type, node2)
+    transaction.merge(relationship)
 
 
 if __name__ == '__main__':
-
     set_working_directory()
 
     graph = Graph(password='test')
-    tx = graph.begin()
+
+    # Use to retrieve existing nodes
+    matcher = NodeMatcher(graph)
 
     with open('Film_Locations_In_San_Francisco.csv') as csv_file:
         reader = DictReader(csv_file)
+        for row in reader:
 
-        films = list_films(reader)
-        locations = list_locations(reader)
-        producers = list_producers(reader)
-        directors = list_directors(reader)
-        writers = list_writers(reader)
-        actors = list_actors(reader)
+            transaction = graph.begin()
 
-        film_nodes = create_films(films)
+            film = find_or_create_node('film', 'title', row['title'])
+            location = find_or_create_node('location', 'name', row['location'])
+            fun_fact = find_or_create_node('fun_fact', 'fun_fact', row['fun_fact'])
+            producer = find_or_create_node('organization', 'name', row['producer'])
+            distributor = find_or_create_node('organization', 'name', row['distributor'])
+            director = find_or_create_node('person', 'name', row['director'])
+            writer = find_or_create_node('person', 'name', row['writer'])
+            actor_1 = find_or_create_node('person', 'name', row['actor_1'])
+            actor_2 = find_or_create_node('person', 'name', row['actor_2'])
+            actor_3 = find_or_create_node('person', 'name', row['actor_3'])
+
+            add_label(producer, 'producer')
+            add_label(distributor, 'distributor')
+            add_label(director, 'director')
+            add_label(writer, 'writer')
+            add_label(actor_1, 'actor')
+            add_label(actor_2, 'actor')
+            add_label(actor_3, 'actor')
+
+            create_relationship(director, 'directed', film)
+            create_relationship(film, 'directed_by', director)
+            create_relationship(distributor, 'distributed', film)
+            create_relationship(film, 'distributed_by', distributor)
+            create_relationship(writer, 'wrote', film)
+            create_relationship(film, 'written_by', writer)
+            create_relationship(actor_1, 'starred_in', film)
+            create_relationship(film, 'starring', actor_1)
+            create_relationship(actor_2, 'starred_in', film)
+            create_relationship(film, 'starring', actor_2)
+            create_relationship(actor_3, 'starred_in', film)
+            create_relationship(film, 'starring', actor_3)
+            create_relationship(producer, 'produced', film)
+            create_relationship(film, 'produced_by', producer)
+            create_relationship(location, 'location_for', film)
+            create_relationship(film, 'filmed_at', location)
+            create_relationship(fun_fact, 'happened_at', location)
+            create_relationship(fun_fact, 'happened_during', film)
+            create_relationship(film, 'has_fact', fun_fact)
+            create_relationship(location, 'has_fact', fun_fact)
+
+            transaction.commit()
 
